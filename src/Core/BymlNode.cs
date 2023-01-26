a7f6637798bea2fbc85dc4283d6a7448a16db285
ArchLeaders <archparliament@gmail.com>
2023-01-26 12:24:05 -0800

    Check string table type identifier before parsing

--- src/Core/BymlNode.cs
+++ src/Core/BymlNode.cs
@@ -1,15 +1,34 @@
 ﻿using CeadLibrary.IO;
-using System.Collections;
-using System.Reflection.PortableExecutable;
 
 namespace BymlLibrary.Core;
+
+public enum NodeType : byte
+{
+    String = 0xA0,
+    Binary = 0xA1,
+    Array = 0xC0,
+    Hash = 0xC1,
+    StringTable = 0xC2,
+    Bool = 0xD0,
+    Int = 0xD1,
+    Float = 0xD2,
+    UInt = 0xD3,
+    Int64 = 0xD4,
+    UInt64 = 0xD5,
+    Double = 0xD6,
+    Null = 0xFF,
+}
 
 public static class BymlNode
 {
     public static void ReadStringTable(this CeadReader reader, long tableOffset, ref List<string> array)
     {
         using (reader.TemporarySeek(tableOffset, SeekOrigin.Begin)) {
-            int size = reader.ReadInt32() & 0x00FFFFFF;
+            if (reader.ReadEnum<NodeType>() != NodeType.StringTable) {
+                throw new InvalidTypeException("Invalid string table node");
+            }
+
+            int size = reader.ReadInt24();
             for (int i = 0; i < size; i++) {
                 uint offset = reader.ReadUInt32();
                 string str = reader.ReadObject(tableOffset + offset, SeekOrigin.Begin, () => reader.ReadString(StringType.ZeroTerminated));
