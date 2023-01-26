ce3378135b5c240d11d8e5b0cb197ebcdc8abcee
ArchLeaders <archparliament@gmail.com>
2023-01-26 12:03:50 -0800

    Impl string table parser

---
+++ src/Core/BymlNode.cs
@@ -0,0 +1,20 @@
+﻿using CeadLibrary.IO;
+using System.Collections;
+using System.Reflection.PortableExecutable;
+
+namespace BymlLibrary.Core;
+
+public static class BymlNode
+{
+    public static void ReadStringTable(this CeadReader reader, long tableOffset, ref List<string> array)
+    {
+        using (reader.TemporarySeek(tableOffset, SeekOrigin.Begin)) {
+            int size = reader.ReadInt32() & 0x00FFFFFF;
+            for (int i = 0; i < size; i++) {
+                uint offset = reader.ReadUInt32();
+                string str = reader.ReadObject(tableOffset + offset, SeekOrigin.Begin, () => reader.ReadString(StringType.ZeroTerminated));
+                array.Add(str);
+            }
+        }
+    }
+}
